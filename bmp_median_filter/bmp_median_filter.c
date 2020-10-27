dbc351f22b80b8aa4b12df72dc5e1810e7b8e17c
Bruno Caregnato <33569494+brunocaregnato@users.noreply.github.com>
2020-10-26 21:52:29 -0300

    corrigido e refatorado! :)

--- bmp_median_filter/bmp_median_filter.c
+++ bmp_median_filter/bmp_median_filter.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 
 #pragma pack(1)
 
+/* Cabecalho da imagem bmp */
 typedef struct cabecalho {
 	unsigned short tipo;
 	unsigned int tamanho_arquivo;
@@ -23,25 +25,68 @@ typedef struct cabecalho {
 	unsigned int cores_importantes;
 } CABECALHO;
 
-typedef struct pixel{
-    unsigned char blue; //char sem sinal para ir de 0 a 256 bytes
+/* Pixeis da imagem bmp 
+ * utiliza char sem sinal para ir de 0 a 256 bytes
+ */
+typedef struct pixel {
+    unsigned char blue; //
     unsigned char green;
     unsigned char red;
 } RGB;
 
-unsigned char **allocate_image(int rows, int colums)
-{
-   unsigned char **image=NULL;
+RGB *allocate_image(FILE *file, CABECALHO *header) {
+   RGB *image = (RGB*) malloc(sizeof(RGB));
 
-   image = (unsigned char **) malloc(rows * sizeof(unsigned char *));
-   
-   for(int row = 0;row < rows;row++)
-      image[row] = (unsigned char *) malloc(colums * sizeof(unsigned char));
+   image = (RGB*) malloc(sizeof(RGB) * (header->altura * header->largura));
 
-   return image;
+   int offset = 0, ali = (header->largura * 3) % 4;
+   char aux;
+
+   if (ali != 0)
+       ali = 4 - ali;
+
+   for (int i = 0; i < header->altura; i++) {
+       fread(image + offset, sizeof(RGB), header->largura, file);
+       offset += header->largura;
+
+       if (ali) fread(&aux, sizeof(unsigned char), ali, file);
+   }
+
+    return image;
 }
 
-void quick_sort(unsigned char *array, int first, int last){
+RGB *copy_image(RGB *image, CABECALHO *header) {
+   RGB *colors = (RGB*) malloc(sizeof(RGB) * (header->largura * header->altura));
+   memcpy(colors, image, sizeof(RGB) * (header->largura * header->altura));
+   return colors;
+}
+
+
+void create_image(FILE *file, RGB *image, CABECALHO header) {    
+   fwrite(&header, sizeof(CABECALHO), 1, file);
+
+   int offset = 0, ali = (header.largura * 3) % 4;
+   char aux;
+
+   if (ali != 0)
+       ali = 4 - ali;
+
+   for (int i = 0; i < header.altura; i++) {
+       fwrite(image + offset, sizeof(RGB), header.largura, file);
+       offset += header.largura;
+       
+       if (ali) fwrite(&aux, sizeof(unsigned char), ali, file);
+   }
+}
+
+RGB *setOffset(RGB *image, CABECALHO header, int row, int col) {
+   if (row < 0 || row > header.altura || col < 0 || col > header.largura)
+      return NULL;
+
+   return image + ((row * header.largura) + col);
+}
+
+void quick_sort(unsigned char *array, int first, int last) {
    int i, j, pivot, temp;
 
    if(first < last) {
@@ -68,61 +113,73 @@ void quick_sort(unsigned char *array, int first, int last){
 }
 
 int main(int argc, char **argv) {
-   unsigned char **image = NULL;
-   FILE *in, *out;
-   CABECALHO c;
-   RGB p;
+   FILE *fileIn, *fileOut;
+   CABECALHO headerIn, headerOut;
+   RGB *imageIn, *imageOut;
     
    if (argc != 4) {
        printf("%s <imagem_de_entrada> <imagem_de_saida> <tamanho_mascara> <numero_threads>\n", argv[0]);
        exit(0);
    }
-   
-   if((in = fopen(argv[1], "rb")) == NULL){
-      printf("Erro ao abrir a imagem de entrada.\n");
+
+   if ((fileIn = fopen(argv[1], "rb")) == NULL) {
+      printf("Nao foi possivel abrir o arquivo de entrada!\n");
       exit(0);
    }
 
-   if((out = fopen(argv[2], "wb")) == NULL) {
-      printf("Erro ao abrir a imagem de saida.\n");
+   if ((fileOut = fopen(argv[2], "wb")) == NULL) {
+      printf("Nao foi possivel abrir o arquivo de saida!\n");
       exit(0);
    }
 
-   fread(&c, sizeof(CABECALHO), 1, in);
-   image = allocate_image(c.altura, c.largura);
-   for (int row = 0;row < c.altura;row++) {
-      fread((image)[row], 1, c.largura, in);
-   }
+   fread(&headerIn, sizeof(CABECALHO), 1, fileIn);
+
+   imageIn = allocate_image(fileIn, &headerIn);
+
+   headerOut = headerIn;
+   imageOut = copy_image(imageIn, &headerIn);
 
    int maskSize = atoi(argv[3]);
+   int size = maskSize * maskSize;
+   unsigned char color_blue[size], color_green[size], color_red[size];
 
-   fwrite(&c, sizeof(CABECALHO), 1, out);     
-   unsigned char *pixel_values=NULL;
- 
-   pixel_values = (unsigned char *) malloc(maskSize * maskSize * sizeof(unsigned char));  
- 
-    for(int row = 0;row < c.altura;row++){
-       fread(&p, sizeof(RGB), 1, in);
-       for(int col = 0;col < c.largura;col++){
-          int point = 0;
-          for(int rowAux = row - (maskSize/2);rowAux < (row-(maskSize/2) + maskSize);rowAux++){
-             for(int colAux = col-(maskSize/2);colAux < (col-(maskSize/2) + maskSize);colAux++){
-                if((rowAux >= 0) && (rowAux < c.altura) && (colAux >= 0) && (colAux < c.largura)){
-                   pixel_values[point] = image[rowAux][colAux];
-                   point++;
-	             }
-             }
+   for (int row = 0;row < headerIn.altura; row++) {
+      for (int col = 0;col < headerIn.largura; col++) {
+         if (row < (maskSize/2) || row >= headerIn.altura - (maskSize/2)
+            || col < (maskSize/2) || col >= headerIn.largura - (maskSize/2)) {                   
+               RGB *pixel = setOffset(imageOut, headerOut, row, col);
+               pixel->red = 0;
+               pixel->green = 0;
+               pixel->blue = 0;
+               continue;
           }
- 
-          quick_sort(pixel_values, 0, point - 1);         
-          p.blue = pixel_values[point/2];
-          p.green = pixel_values[point/2];
-          p.red = pixel_values[point/2];
-          fwrite(&p, sizeof(RGB), 1, out);
-       }
-    } 		
-   
-   fclose(out);
-   fclose(in);
-   free(image);
+
+         int point = 0;
+         for (int rowAux =- (maskSize/2);rowAux <= (maskSize/2); rowAux++) {
+            for (int colAux =- (maskSize/2);colAux <= (maskSize/2); colAux++) {               
+               RGB *pixel = setOffset(imageIn, headerIn, row + rowAux, col + colAux);
+               color_blue[point] = pixel->blue;
+               color_green[point] = pixel->green;
+               color_red[point] = pixel->red;
+               point++;
+            }
+         }
+
+         RGB *pixel = setOffset(imageOut, headerOut, row, col);
+
+         quick_sort(color_blue, 0, point - 1);
+         pixel->blue = color_blue[size/2];
+         quick_sort(color_green, 0, point - 1);
+         pixel->green = color_green[size/2];
+         quick_sort(color_red, 0, point - 1);
+         pixel->red = color_red[size/2];
+      }
+   }   
+
+   create_image(fileOut, imageOut, headerOut);
+
+   fclose(fileIn);
+   fclose(fileOut);
+   free(imageIn);
+   free(imageOut);
 }
